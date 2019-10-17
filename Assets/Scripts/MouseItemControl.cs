c961e331aeb4f99b2c8c70c55c9ea1b376487e4f
Sunny Peebles <sunny.peebles@gmail.com>
2019-10-17 17:17:58 +1300

    Fonts Removed. Adding Mouse Graphics.Raycast-ing

--- Assets/Scripts/MouseItemControl.cs
+++ Assets/Scripts/MouseItemControl.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class MouseItemControl : MonoBehaviour
 {
     public InventoryItem mouseItem;
     public InventoryBase invBase;
 
+    public Canvas inventoryCanvas;
+
+    GraphicRaycaster raycaster;
+    PointerEventData pointerEventData;
+    EventSystem eventSystem;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        raycaster = inventoryCanvas.GetComponent<GraphicRaycaster>();
+        eventSystem = GetComponent<EventSystem>();
     }
 
     private void Awake()
@@ -33,6 +42,29 @@ public class MouseItemControl : MonoBehaviour
 
     void PickUpItem()
     {
+        // On a left click
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Are we hovering over an inventory item
+            
 
+            foreach(RaycastResult result in results)
+            {
+
+            }
+        }
+    }
+
+    List<RaycastResult> GetNewPointerEventRaycast()
+    {
+        pointerEventData = new PointerEventData(eventSystem);
+
+        pointerEventData.position = Input.mousePosition;
+
+        List<RaycastResult> results = new List<RaycastResult>();
+
+        raycaster.Raycast(pointerEventData, results);
+
+        return results;
     }
 }
